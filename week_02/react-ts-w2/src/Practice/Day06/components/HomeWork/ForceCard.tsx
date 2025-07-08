0dd6d11e0aaf46555e75dc093e1484a9daefbde6
Nhan121631 <phamphunhanpham8@gmail.com>
2025-07-08 10:26:05 +0700

    day 6 home w

--- week_02/react-ts-w2/src/Practice/Day06/components/HomeWork/ForceCard.tsx
+++ week_02/react-ts-w2/src/Practice/Day06/components/HomeWork/ForceCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 type Props = {
-  weatherDatas?:any;
+  weatherDatas?: any;
 };
 
 export const ForceCard: React.FC<Props> = ({ weatherDatas }) => {
@@ -14,18 +14,21 @@ export const ForceCard: React.FC<Props> = ({ weatherDatas }) => {
   const now = new Date();
   const nowHour = now.getHours();
 
-  // Lấy 4 mốc giờ liên tiếp bắt đầu từ giờ hiện tại
   const nearest4 = allHours.slice(nowHour, nowHour + 4);
 
   return (
     <div className="flex justify-center gap-2 mt-6 bg-white rounded-2xl p-4">
-      {nearest4.map((h:any, idx:any) => (
+      {nearest4.map((h, idx) => (
         <div
           key={idx}
           className=" rounded-xl p-3 flex flex-col items-center w-30 h-36"
         >
           <div className="font-semibold">{h.time.slice(-5)}</div>
-          <img src={h.condition.icon} alt={h.condition.text} className="w-10 h-10" />
+          <img
+            src={h.condition.icon}
+            alt={h.condition.text}
+            className="w-10 h-10"
+          />
           <div className="text-lg font-bold">{h.temp_c}°C</div>
           <div className="text-xs">{h.condition.text}</div>
         </div>
